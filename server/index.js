7c759c5d2c4b736648e4ff19a9416caf285c3526
Hr-AHTPL <hr@ahtpl.in>
2025-11-21 14:58:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,6 +26,20 @@ app.use('/medicine', medicineRouter)
 app.use('/api/sale', salesRouter);
 app.use('/api/purchase', purchaseRouter);
 app.use('/api/closing-stock', closingStockRouter);
+
+const cors = require('cors');
+
+app.use(cors({
+  origin: [
+    'https://bhartiyadharohar.in',
+    'https://www.bhartiyadharohar.in',
+    'http://localhost:5173',
+    'http://localhost:3000'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 // connect to mongoDB
 mongoose.connect(process.env.DBURL).then(()=>{
     console.log('connected to mongodb');
