a3a6cce07eefca51110cf6285725a47d51fb3f1b
Hr-AHTPL <hr@ahtpl.in>
2026-02-06 17:12:58 +0530

    Update userDetails.model.js

--- server/App/models/userDetails.model.js
+++ server/App/models/userDetails.model.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true, enum: ['doctor', 'receptionist'] }
+  role: { type: String, required: true, enum: ['doctor', 'receptionist', 'admin'] }
 });
 
 module.exports = mongoose.model('staffDetail', userSchema);
